ae28387f6de833d7ff8fa82912cfe49f0ac81a4c
John Nolcox <jnolcox0429+github@gmail.com>
2017-09-06 17:13:45 -0400

    changed toString method

--- PersonTest.java
+++ PersonTest.java
@@ -3,6 +3,19 @@ public class PersonTest {
         Person p1 = new Person(18, "John");
         Person p2 = new Person(25, "Ashley");
         OrderedPair <Person> oi = new OrderedPair<>(p1,p2);
-        System.out.println("1: " + oi.getFirst().getName() + " " + oi.getFirst().getAge());
+
+        System.out.println(p1);
+        System.out.println(p2);
+
+//        System.out.println("1: " + oi.getFirst().getName() + " " + oi.getFirst().getAge());
+//        System.out.println("2: " + oi.getSecond().getName() + " " + oi.getSecond().getAge());
+
+        oi.changeOrder();
+        System.out.println("After changing the order:");
+
+        System.out.println(p1);
+        System.out.println(p2);
+//        System.out.println("1: " + oi.getFirst().getName() + " " + oi.getFirst().getAge());
+//        System.out.println("2: " + oi.getSecond().getName() + " " + oi.getSecond().getAge());
     }
 }
